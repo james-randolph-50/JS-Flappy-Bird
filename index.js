616142a46bf40db63eb9b5a15abd29ff4b1d140b
James <james.randolph2016@gmail.com>
2019-03-25 17:17:54 -0400

    gravity, bird posiitons, pipe positions add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var cvs = document.getElementById('canvas');
 var ctx = cvs.getContext('2d');
 
+
 // Load Images
 
 var bird = new Image();
@@ -17,14 +18,37 @@ fg.src = "images/fg.png";
 pipeTop.src = "images/pipeTop.png";
 pipeBottom.src = "images/pipeBottom.png";
 
-// Draw Images
+// pipe vars
 
-bird.onload = function draw() {
+var gap = 100;
+var constant = pipeTop.height+gap;
+
+// bird vars
+
+var bX = 10;
+var bY = 150;
+
+// Gravity
+
+var gravity = 1;
+
+// draw images
+
+window.onload = function draw() {
+    
+
 
     ctx.drawImage(bg,0,0);
 
     ctx.drawImage(pipeTop, 100, 0);
     ctx.drawImage(pipeBottom, 100, 0+constant);
+    ctx.drawImage(fg,0, cvs.height - fg.height);
+
+    ctx.drawImage(bird, bX, bY);
+
+    bY += gravity;
+
+    requestAnimationFrame(draw);
 };
 
 draw();
