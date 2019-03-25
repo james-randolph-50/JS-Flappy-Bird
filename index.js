f8eec395b35fad81dc8cb0f633507db0e007d1c2
James <james.randolph2016@gmail.com>
2019-03-25 16:16:50 -0400

    add image variables

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var cvs = document.getElementById('canvas');
 var ctx = cvs.getContext('2d');
 
 // Load Image
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeTop = new Image();
+var pipeBottom = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeTop.src = "images/pipeTop.png";
+pipeBottom.src = "images/pipeBottom.png";
+
 var imageName = new Image();
 imageName.src = "images/img.png";
 
