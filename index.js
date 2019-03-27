d445782306e91977ad935b611a3bf74aadf23e93
James <james.randolph2016@gmail.com>
2019-03-27 11:44:15 -0400

    removed preloading, changed variale placement. program working now

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-// Canvas and getContext
 
-var cvs = document.getElementById('canvas');
-var ctx = cvs.getContext('2d');
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
 
-// Load Images
-
-// function preloadImage(url) {
-//     var img = new Image();
-//     img.src = url;
-// }
-
-// preloadImage(
-//     "images/pipeBottom.png",
-//     "images/pipeTop.png",
-//     "images/fg.png",
-//     "images/bg.png",
-//     "images/bird.png"
-// )
+// load images
 
 var bird = new Image();
 var bg = new Image();
@@ -30,86 +16,93 @@ fg.src = "images/fg.png";
 pipeTop.src = "images/pipeTop.png";
 pipeBottom.src = "images/pipeBottom.png";
 
-// pipe vars
 
-var gap = 100;
-var constant = pipeTop.height+gap;
-var pipe = [];
+// some variables
 
-    pipe[0] = {
-        x : cvs.width,
-        y : 0
-    }
-
-// bird vars
+var gap = 85;
+var constant;
 
 var bX = 10;
 var bY = 150;
 
-// World Vars
-
 var gravity = 1;
+
 var score = 0;
 
-// Audio Files
+// audio files
 
 var fly = new Audio();
-var score = new Audio();
+var scor = new Audio();
 
-fly.src = "sounds/fly.mp3"
-score.src = "sounds/score.mp3"
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
 
-// Pressing Keys
+// on key down
 
-document.addEventListener("keydown", moveUp)
+document.addEventListener("keydown",moveUp);
 
-function moveUp() {
-    bY -= 30;
+function moveUp(){
+    bY -= 35;
     fly.play();
 }
 
+// pipe coordinates
+
+var pipe = [];
+
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+};
+
 // draw images
 
-window.onload = function draw() {
-
+function draw(){
+    
     ctx.drawImage(bg,0,0);
-
-    for (var i = 0; i < pipe.length; i++){
-        ctx.drawImage(pipeTop, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y+constant);
-
+    
+    
+    for(var i = 0; i < pipe.length; i++){
+        
+        constant = pipeTop.height+gap;
+        ctx.drawImage(pipeTop,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeBottom,pipe[i].x,pipe[i].y+constant);
+             
         pipe[i].x--;
-
-        if (pipe[i].x == 125){
+        
+        if( pipe[i].x == 125 ){
             pipe.push({
                 x : cvs.width,
                 y : Math.floor(Math.random()*pipeTop.height)-pipeTop.height
-            });
+            }); 
         }
+
+        // detect collision
+        
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeTop.width && (bY <= pipe[i].y + pipeTop.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+            location.reload(); // reload the page
+        }
+        
+        if(pipe[i].x == 5){
+            score++;
+            scor.play();
+        }
+        
+        
     }
 
-    // Detect Collision
-
-    if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeTop.width && (bY <= pipe[i].y + pipeTop.height || bY+bird.height >= pipe[i].y+constant || bY + bird.height >= cvs.height - fg.height)){
-        location.reload();
-    }
-
-    if (pipe[i].x == 5) {
-        score+= 5;
-        score.play();
-    }
-
-    ctx.drawImage(fg,0, cvs.height - fg.height);
-
-    ctx.drawImage(bird, bX, bY);
-
+    ctx.drawImage(fg,0,cvs.height - fg.height);
+    
+    ctx.drawImage(bird,bX,bY);
+    
     bY += gravity;
-
+    
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
-    ctx.fillText("Score: "+ score, 10, cvs.height-20);
-
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+    
     requestAnimationFrame(draw);
-};
+    
+}
 
 draw();
